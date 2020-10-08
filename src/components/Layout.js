8d65f35965582cda2164e5ff2040b0c0f73a7683
James Vickery <dev@jamesvickery.net>
2020-10-08 02:21:35 +0100

    more stylez - recipe page container etc

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,17 +2,12 @@ import React from "react"
 import PropTypes from "prop-types"
 import Nav from "../components/Nav"
 import Footer from "../components/Footer"
-import "./Layout.scss"
 
 const Layout = ({ children }) => {
   return (
     <>
       <Nav />
-
-      <div className="container main-content">
-        <main>{children}</main>
-      </div>
-
+      {children}
       <Footer />
     </>
   )
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,33 +1,40 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
-import Nav from "../components/Nav"
+
+import Layout from "../components/Layout"
+
+import "../styles/post-template.scss"
 
 export default ({ data }) => {
   const post = data.markdownRemark
 
   return (
-    <div>
-      <Nav post={true} />
+    <Layout>
       <div className="post-page-container">
-        <div className="post-page-flex-container">
-          <div className="post-image-container">
-            <Img
-              className="post-image"
-              sizes={post.frontmatter.image.childImageSharp.sizes}
-            />
-          </div>
+        <div className="post-image-container">
+          <Img
+            className="post-image"
+            sizes={post.frontmatter.image.childImageSharp.sizes}
+          />
+        </div>
 
+        <div className="container">
           <div className="post-content-container">
-            <h1>{post.frontmatter.title}</h1>
-            <h4 style={{ color: "rgb(165, 164, 164)", fontSize: "0.8em" }}>
-              {post.frontmatter.date} - {post.frontmatter.cookTime} cooking time
-            </h4>
+            <div className="post-details">
+              <h1>{post.frontmatter.title}</h1>
+
+              <h4 style={{ color: "rgb(165, 164, 164)", fontSize: "0.8em" }}>
+                {post.frontmatter.date} - {post.frontmatter.cookTime} cooking
+                time
+              </h4>
+            </div>
+
             <div dangerouslySetInnerHTML={{ __html: post.html }} />
           </div>
         </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
@@ -41,7 +48,7 @@ export const query = graphql`
         cookTime
         image {
           childImageSharp {
-            sizes(maxWidth: 630) {
+            sizes(maxWidth: 1920) {
               ...GatsbyImageSharpSizes
             }
           }
